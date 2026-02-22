684f4444f5c7ad28b9240d2b81b37c7cac84b60d
andrei <andrei.tudor10000@gmail.com>
2026-02-22 22:39:58 +0200

    This time it actually kills some tasks, but they are hardcoded. Maybe I should implement killing child tasks as well. First I need to get the tasks from a file instead. Also removed some unneeded libraries.

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
-#include <unordered_map>
 #include <map>
+
+#include <chrono>
 
 #include <windows.h>
 #include <tlhelp32.h>
 #include <tchar.h>
 
-struct Process {
-  TCHAR name;
-  DWORD id;
-};
+std :: multimap<std :: string, unsigned long> processMap;
 
-bool sortAlphabetically(std :: string a, std :: string b);
-
-void KillProcessesByName(std :: vector<std :: string> tasks);
+void killProcessesByName(std :: vector<std :: string> tasks);
 
 int main() {
+  clock_t main_start_time { clock() };
+
   std :: vector<std :: string> tasksk;
   tasksk.push_back(std :: string("a.exe"));
   tasksk.push_back(std :: string("b.exe"));
@@ -29,7 +26,9 @@ int main() {
   tasksk.push_back(std :: string("g.exe"));
   tasksk.push_back(std :: string("h.exe"));
 
-  KillProcessesByName(tasksk);
+  killProcessesByName(tasksk);
+  
+  std :: cout << "Total program time: " << float (clock() - main_start_time) << "\n";
   
   // Sort array alphabetically
   // std :: sort(tasksk.begin(), tasksk.end(), sortAlphabetically);
@@ -38,7 +37,9 @@ int main() {
 }
 
 
-void KillProcessesByName(std :: vector<std :: string> tasks) {
+void killProcessesByName(std :: vector<std :: string> tasks) {
+  clock_t begin_time { clock() };
+
   HANDLE processes_snapshot { CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0) };
   PROCESSENTRY32 process {};
   process.dwSize = sizeof(PROCESSENTRY32);
@@ -49,24 +50,35 @@ void KillProcessesByName(std :: vector<std :: string> tasks) {
     return;
   }
 
-  std :: multimap<std :: string, unsigned long> processMap;
-
+  processMap.clear();
+  
   while (processes_remaining) {
     processMap.insert({process.szExeFile, process.th32ProcessID});
 
     processes_remaining = Process32Next(processes_snapshot, &process);
   }
 
-  for (const auto &elem: processMap) {
-    std :: cout << elem.first << " -> " << elem.second << "\n";
+  std :: cout << "Time for getting processes: " << float (clock() - begin_time) << "\n";
+
+  // for (const auto &elem: processMap) {
+  //   std :: cout << elem.first << " -> " << elem.second << "\n";
+  // }
+
+  begin_time = clock();
+
+  for (const auto &task: tasks) {
+    auto pids = processMap.equal_range(task);
+    for (auto it = pids.first; it != pids.second; it ++) {
+      const auto kprocess = OpenProcess(PROCESS_TERMINATE, false, it -> second);
+      if (kprocess) {
+        TerminateProcess(kprocess, 1);
+        CloseHandle(kprocess);
+      }
+    }
   }
-  std :: cout << "\n";
+
+  std :: cout << "Time for killing processes: " << float (clock() - begin_time) << "\n";
+  
+  CloseHandle(processes_snapshot);
 }
-
-bool sortAlphabetically(std :: string a, std :: string b) {
-  return a < b;
-}
-
-
-
 
